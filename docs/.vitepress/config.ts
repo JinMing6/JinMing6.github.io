cb17acac675a1b135145a3d46605c9fb60651748
JinMing6 <response965@163.com>
2023-06-22 18:33:47 +0800

    for build

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: "/docs/",
 	title: "Jinming6",
 	description: "个人博客",
 	themeConfig: {
