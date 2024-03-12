168c9215333eab631d0f4f5c82c7cbae31d04d3a
JinMing6 <response965@163.com>
2024-03-12 15:09:51 +0800

    fix: lottie无法显示

--- docs/components/DemoLottie.vue
+++ docs/components/DemoLottie.vue
@@ -6,9 +6,7 @@ import { DotLottieVue } from '@lottiefiles/dotlottie-vue';
   <div>
     <DotLottieVue
       autoplay
-      controls
       loop
-      mode="normal"
       src="/Animation - 1710222853150.json"
       style="height: 320px; width: 320px"
     >
